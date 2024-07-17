f3c345133a06be785a8f0f58fcf4fdc79307824c
Adeeb Raheja <adeebraheja1993@gmail.com>
2024-07-17 13:46:16 -0400

    first commit

---
+++ src/test/java/GitPractice_July12/PracticeSessionOne_July12Test.java
@@ -0,0 +1,12 @@
+package GitPractice_July12;
+
+import org.testng.annotations.Test;
+
+public class PracticeSessionOne_July12Test {
+	
+	
+	@Test
+	public void methodOne() {
+		System.out.println("method One");
+	}
+}
